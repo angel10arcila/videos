eaede2c3f91853bf89f4576fcbd482466d615cf7
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-07-05 12:29:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const mediaFiles = [
    },
 
    {
+      name: "©Mlb - Resumen HOU vs LAD",
+      url: "https://mlb-cuts-diamond.mlb.com/FORGE/2025/2025-07/04/6e9c6c5f-db296f3c-e9a3df84-csvm-diamondgcp-asset.m3u8",
+      type: "Video"
+   },
+   
+   {
       name: "©Mlb - Resumen BAL vs ATL",
       url: "https://mlb-cuts-diamond.mlb.com/FORGE/2025/2025-07/04/70828f4b-02b853a4-c56e98ce-csvm-diamondgcp-asset.m3u8",
       type: "Video"
