1ba777927d998d94a12a3024b0d729e0ca090fa4
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-07-06 20:10:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ const mediaFiles = [
    
    {
       name: "©Mlb - José Altuve",
+      url: "https://mlb-cuts-diamond.mlb.com/FORGE/2025/2025-07/06/178a634b-43203bf9-ccedf9de-csvm-diamondgcp-asset.m3u8",
+      type: "Video"
+   },
+   
+   {
+      name: "©Mlb - José Altuve",
       url: "https://mlb-cuts-diamond.mlb.com/FORGE/2025/2025-07/06/c07da7b5-dc94074a-8ab7b19d-csvm-diamondgcp-asset.m3u8",
       type: "Video"
    },
